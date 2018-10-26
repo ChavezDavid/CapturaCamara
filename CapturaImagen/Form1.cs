3767fd6a404c6b440c27500b632a3fcfe87e1c30
David Chavez <david_12-95@hotmail.com>
2018-10-26 11:35:20 -0700

    Add project files.

---
+++ CapturaImagen/Form1.cs
@@ -0,0 +1,87 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+using Emgu.CV;
+using Emgu.CV.Structure;
+
+namespace CapturaImagen
+{
+    public partial class Form1 : Form
+    {
+        VideoCapture capture;
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void startToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            if(capture == null)
+            {
+                capture = new Emgu.CV.VideoCapture(0);
+            }
+            capture.ImageGrabbed += CaptureImageGrabbed;
+            capture.Start();
+        }
+
+        private void CaptureImageGrabbed(object sender, EventArgs e)
+        {
+            try
+            {
+                Mat m = new Mat();
+                capture.Retrieve(m);
+                pictureBox1.Image = m.ToImage<Bgr, byte>().Bitmap;
+            }
+
+            catch(Exception)
+            {
+
+            }
+        }
+
+        private void stopToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            if (capture != null)
+            {
+                capture.Stop();
+            }
+        }
+
+        private void startToolStripMenuItem1_Click(object sender, EventArgs e)
+        {
+            OpenFileDialog ofd = new OpenFileDialog();
+            ofd.Filter = "Videos tipo | *.mp4";
+            if (ofd.ShowDialog() == DialogResult.OK)
+            {
+                capture = new Emgu.CV.VideoCapture(ofd.FileName);
+            }
+            capture.ImageGrabbed += CaptureImageGrabbed1;
+            capture.Start();
+        }
+
+        private void CaptureImageGrabbed1(object sender, EventArgs e)
+        {
+            try
+            {
+                Mat m = new Mat();
+                capture.Retrieve(m);
+                pictureBox1.Image = m.ToImage<Bgr, byte>().Bitmap;
+                Thread.Sleep(1);
+            }
+
+            catch (Exception)
+            {
+
+            }
+        }
+    }
+}
